d3d9fc4f3af57b008aa867b90a5ed8622382aa47
Vincent Surya Bensawan <bensawan0298@gmail.com>
2019-05-17 06:31:30 -0700

    Edited the manifest to allow internet access

--- app/src/main/java/com/cse110easyeat/api/service/GooglePlacesAPIServices.java
+++ app/src/main/java/com/cse110easyeat/api/service/GooglePlacesAPIServices.java
@@ -1,38 +1,53 @@
 package com.cse110easyeat.api.service;
 
-import com.android.volley.toolbox.Volley;
 import com.cse110easyeat.easyeat;
 import android.permission.INTERNET;
+import com.squareup.okhttp.*;
 
 import com.cse110easyeat.easyeat.BuildConfig;
-import com.cse110easyeat.easyeat.MainActivity;
 import android.content.Context;
+import android.util.Log;
 
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
 
 public class GooglePlacesAPIServices implements APIHandlerService {
-    private PlacesClient googleClient;
-    private Context context;
+    private static final String TAG = "GooglePlacesAPIServices";
 
+    private static final String apiRequestURL = "https://maps.googleapis.com/maps/api/place" +
+            "/textsearch/json?&query=%s&key=%s&location=%f,%f&radius=%f";
 
-    public void initializeAPI(Context applicationContext) {
-        // TODO:HARD CODING THE API KEY
-        context = applicationContext;
-        Places.initialize(applicationContext, BuildConfig.PLACES_API_KEY);
-        googleClient = Places.createClient(context);
+    public void initializeAPIClient() {
+
     }
 
-    public String getRestaurantInfo() {
+    // add minprice, maxprice opennow
+    // Check if price is an option
+    public String getRestaurantInfo(String queryString, int minPrice, int maxPrice,
+                                    float latitude, float longitude, float radius) {
         String result = "";
+        /* Replace whitespace with plus buttons */
+        String modifiedQueryString = queryString.replaceAll("\\s+","+");
+        String url = String.format(apiRequestURL, queryString, BuildConfig.PLACES_API_KEY,
+                latitude, longitude, radius);
 
-        int price = 2;
-        float miles = 2f;
+        if (minPrice > 0 && minPrice <= 4) {
+            url += "&minPrice=";
+            url += minPrice;
+        }
 
-        float metersPerMile = 1609.34f;
+        if (maxPrice > 0 && maxPrice <= 4) {
+            url += "&maxPrice=";
+            url += maxPrice;
+        }
 
-        RequestQueue queue = Volley.newRequestQueue(context).;
-        String url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters";
+        // Send the payload
+//        try {
+//            Request apiRequest = new Request.Builder().url(url).build();
+//            Response apiResponse = apiClientHandler.newCall(apiRequest).execute();
+//            result = apiResponse.body().toString();
+//            Log.i(TAG, "Api call result" + result);
+//        } catch(Exception e) {
+//            Log.e(TAG, "Exception when calling api" + e.toString());
+//        }
 
         return result;
     }
